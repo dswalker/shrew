f04bacc96c478ffe29aff733b6bf20f2694758e8
David Walker <dwalker@calstate.edu>
2013-09-06 13:30:29 -0700

    composer warning

--- sierra/test.php
+++ sierra/test.php
@@ -2,8 +2,12 @@
 
 ini_set('memory_limit', -1);
 
-require_once 'File/Marc.php';
-require_once 'lib/Sierra.php';
+$autoloader = include_once("vendor/autoload.php");
+
+if ( ! $autoloader ) 
+{
+	echo "\n\n  vendor/autoload.php could not be found. Did you run `php composer.phar install`?\n\n"; exit;
+}
 
 $sierra = new Sierra('sierra-db.your.edu', 'db_user', 'db_pass');
 
